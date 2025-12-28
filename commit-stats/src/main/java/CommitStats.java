9e3c619b49cabd32a457c9bf2b08d984f3225524
AlexandrAnatoliev <per-1986@list.ru>
2025-12-28 13:31:23 +0300

    fix(v0.7.0): check commit-stats by deepseek

--- commit-stats/src/main/java/CommitStats.java
+++ commit-stats/src/main/java/CommitStats.java
@@ -1,21 +1,19 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
-* The class to get git stats 
-*
-* @version  0.7.0
-* @since    28.12.2025
-* @author   AlexandrAnatoliev
-*/
+ * The class to get git stats 
+ *
+ * @version  0.7.0
+ * @since    28.12.2025
+ * @author   AlexandrAnatoliev
+ */
 public class CommitStats {
     /**
-    * Get last commit hash
-    * 
-    * @throws Exception If is error 
-    * @return Last commit hash, or "" if error
-    */
+     * Get last commit hash
+     * 
+     * @return Last commit hash, or "" if error
+     * @throws Exception If an error occurs 
+     */
     public String getLastCommitHash() throws Exception {
         Process p = Runtime.getRuntime().exec("git rev-parse HEAD");
         Scanner s = new Scanner(p.getInputStream());
