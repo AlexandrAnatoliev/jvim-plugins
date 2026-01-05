9aa5ee92ce2159e7c26bac0804424cc33fe7257c
AlexandrAnatoliev <per-1986@list.ru>
2026-01-05 20:46:57 +0300

    fix: test commit

--- commit-stats/src/main/java/Main.java
+++ commit-stats/src/main/java/Main.java
@@ -9,9 +9,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length > 0 && "start".equals(args[0])) {
             start();
-        }
-
-        else if (args.length > 0 && "update".equals(args[0])) {
+        } else if (args.length > 0 && "update".equals(args[0])) {
             update();
         }
     }
@@ -28,7 +26,7 @@ public class Main {
         LocalDate today = LocalDate.now();
 
         if (!commitStats.fileIsExists(PATH_TO_DAILY_COMMITS)
-                || commitStats.getFileDate(PATH_TO_DAILY_COMMITS)
+                || !commitStats.getFileDate(PATH_TO_DAILY_COMMITS)
                 .equals(today)) {
             commitStats.writeDailyCommitsToFile(0L);
                 }
