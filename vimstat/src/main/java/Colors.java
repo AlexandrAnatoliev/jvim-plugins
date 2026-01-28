9d2388ed76dacfc965f49f1a63c5a927f7bad695
AlexandrAnatoliev <per-1986@list.ru>
2026-01-28 10:58:08 +0300

    fix(vimstat): add source code

---
+++ vimstat/src/main/java/Colors.java
@@ -0,0 +1,35 @@
+/**
+ * Enum representation supported ANSI color codes for console output.
+ *
+ * @version  0.7.6
+ * @since    12.01.2026
+ * @author   AlexandrAnatoliev
+ */
+public enum Colors {
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    /** Reset ANSI color code */
+    RESET("\u001B[0m");
+
+    protected final String code;
+
+    Colors(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+
+    /** 
+     * Convenience method to colorize text
+     *
+     * @param   text    The text to colorize
+     * @return  String  Colorized text with reset code appended
+     */
+    public String apply(String text) {
+        return this + text + RESET;
+    }
+}
---
+++ vimstat/src/main/java/CommitStats.java
@@ -0,0 +1,167 @@
+import java.util.Scanner;
+import java.io.*;
+import java.time.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+
+/**
+ * The class to get git stats 
+ *
+ * @version  0.7.6
+ * @since    11.01.2026
+ * @author   AlexandrAnatoliev
+ */
+public class CommitStats {
+    protected String pathToLastCommitHash;
+    protected String pathToDailyCommits;
+
+    /**
+     * CommitStats class constructor
+     *
+     * @param  pathToLastCommitHash Path to temporary file 
+     *                              for last commit hash storage
+     */
+    public CommitStats(
+            String pathToLastCommitHash,
+            String pathToDailyCommits) {
+        this.pathToLastCommitHash = pathToLastCommitHash;
+        this.pathToDailyCommits = pathToDailyCommits;
+            }
+
+    /**
+     * Get last commit hash
+     * 
+     * @return Last commit hash, or "" if error
+     */
+    public String getLastCommitHash() {
+        ProcessBuilder pb = new ProcessBuilder("git", "rev-parse", "HEAD");
+        try {
+            Process p = pb.start();
+            p.waitFor();
+            try (Scanner scanner = new Scanner(p.getInputStream())) {
+                return scanner.hasNext() ? scanner.next() : "";
+            }
+        } catch (Exception e) {
+            System.out.println(Colors.RED.apply("[ERROR]") 
+                    + " get last commit hash: " 
+                    + e.getMessage());
+            return "";
+        }
+    }
+
+    /**
+     * Writes hash to a temporary file
+     *
+     * @param hash Hash to write to the file
+     */
+    public void writeHashToFile(String hash) {
+        try {
+            String content = (hash == null) ? "" : hash;
+            Files.writeString(Paths.get(pathToLastCommitHash), content);
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " writing hash: "
+                    + e.getMessage());
+        }
+    }
+
+    /**
+     * Reads hash value from temporary file 
+     *
+     * @return String Hash value from file 
+     */
+    public String readHashFromFile() {
+        try {
+            return Files.readString(Paths.get(this.pathToLastCommitHash));
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " reading hash: "
+                    + e.getMessage());
+            return "";
+        }
+    }
+
+    /**
+     * Writes daily commits value to temporary file 
+     *
+     * @param  value Daily commits value to write to file
+     */
+    public void writeDailyCommitsToFile(Long value) {
+        try (FileWriter writer = new FileWriter(pathToDailyCommits)) {
+            writer.write(value.toString());
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " writing daily commits: "
+                    + e.getMessage());
+        }
+    }
+
+    /**
+     * Reads daily commits value from temporary file 
+     *
+     * @return  daily commits value from file, 
+     *          or 0 if file does not exist or contains invalid data
+     */
+    public long readDailyCommitsFromFile() {
+        try (BufferedReader reader = new BufferedReader(
+                    new FileReader(this.pathToDailyCommits))) {
+            long value = Long.parseLong(reader.readLine());
+            return value;
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " reading daily commits: "
+                    + e.getMessage());
+        }
+        return 0;
+    }
+
+    /**
+     * Checks if temporary file is exist 
+     * 
+     * @param   pathToFile  Path to temporary file
+     * @return  true        If file is exist
+     *          false       If file is not exist
+     */
+    public boolean isFileExists(String pathToFile) {
+        String homeDir = System.getProperty("user.home");
+        File file = new File(homeDir + pathToFile);
+        try {
+            return file.exists();
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " file existing checking: "
+                    + e.getMessage());
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves the creation date of the file
+     *
+     * @param   pathToFile  Path to temporary file
+     * @return  LocalDate   Representing file creation time
+     */
+    public LocalDate getFileDate(String pathToFile) {
+        String homeDir = System.getProperty("user.home");
+        File file = new File(homeDir + pathToFile);
+        try {
+            BasicFileAttributes attrs = Files.readAttributes(
+                    file.toPath(), 
+                    BasicFileAttributes.class);
+            LocalDate fileDate = attrs.creationTime().toInstant()
+                .atZone(ZoneId.systemDefault()).toLocalDate();
+            return fileDate;
+        } catch (Exception e) {
+            System.out.println(
+                    Colors.RED.apply("[ERROR]")
+                    + " get file date checking: "
+                    + e.getMessage());
+        }
+        return null;
+    }
+}
