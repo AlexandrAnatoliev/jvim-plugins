1a5405e19c6f9ef9cb37171cc86b9504222810ab
AlexandrAnatoliev <per-1986@list.ru>
2026-02-01 11:56:55 +0300

    fix(vimstat): unite timer and commit-stats plugins in vimstat
    version 0.8.5

--- vimstat/src/main/java/Main.java
+++ vimstat/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.time.*;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Vim utility to get commit stats
@@ -11,8 +12,8 @@ import java.time.*;
  *   java Main update   -   update commit stats 
  *   java Main stop     -   print commit stats
  *
- * @version  0.8.0
- * @since    28.01.2026
+ * @version  0.8.5
+ * @since    01.02.2026
  * @author   AlexandrAnatoliev
  */
 public class Main {
@@ -20,6 +21,15 @@ public class Main {
         "/.vim/pack/my-plugins/start/vimstat/data/last_commit_hash.txt";
     private static final String PATH_TO_DAILY_COMMITS = 
         "/.vim/pack/my-plugins/start/vimstat/data/daily_commits.txt";
+
+    private static final String SESSION_FILE_PATH = 
+        "/.vim/pack/my-plugins/start/vimstat/data/jvim_session_time.txt";
+    private static final String DAY_FILE_PATH = 
+        "/.vim/pack/my-plugins/start/vimstat/data/jvim_day_time.txt";
+    private static final String MONTH_FILE_PATH = 
+        "/.vim/pack/my-plugins/start/vimstat/data/jvim_month_time.txt";
+    private static final String YESTERDAY_FILE_PATH = 
+        "/.vim/pack/my-plugins/start/vimstat/data/jvim_yesterday_time.txt";
 
     /** Main entry point for the Commit Stats application
      *
@@ -65,6 +75,49 @@ public class Main {
         if (!commitStats.isFileExists(PATH_TO_LAST_COMMIT_HASH)) {
             commitStats.writeHashToFile("");
         }
+
+        String homeDir = System.getProperty("user.home");
+        String pathToDayTime = homeDir + DAY_FILE_PATH;
+        String pathToMonthTime = homeDir + MONTH_FILE_PATH;
+        String pathToYesterdayTime = homeDir + YESTERDAY_FILE_PATH;
+
+        Timer sessionTimer = new Timer(homeDir + SESSION_FILE_PATH);
+        Timer dayTimer = new Timer(pathToDayTime);
+        Timer monthTimer = new Timer(pathToMonthTime);
+        Timer yesterdayTimer = new Timer(pathToYesterdayTime);
+
+        if(!sessionTimer.fileIsNotExist()) {
+            long pastDuration = sessionTimer.getSessionTime();
+            long dayTime = dayTimer.readFromFile();
+            dayTimer.writeToFile(dayTime + pastDuration);
+        }
+        sessionTimer.writeToFile(System.currentTimeMillis() / 1000);
+
+        if(yesterdayTimer.fileIsNotExist()) {
+            yesterdayTimer.writeToFile(0L);
+        }
+
+        if(monthTimer.fileIsNotExist()) {
+            monthTimer.writeToFile(0L);
+        }
+
+        if(!monthTimer.getFileDate().equals(today)) {
+            long yesterdayTime = monthTimer.readFromFile();
+            yesterdayTimer.writeToFile(yesterdayTime);
+
+            long emptyDays = ChronoUnit.DAYS.between(
+                    monthTimer.getFileDate(), today);
+            long monthTime = monthTimer.readFromFile() * (30 - emptyDays);
+            monthTimer.writeToFile(
+                    (monthTime + dayTimer.readFromFile()) / 30);     
+        }
+
+        if(dayTimer.fileIsNotExist() || 
+                !dayTimer.getFileDate().equals(today)) {
+            dayTimer.writeToFile(0L);
+                }
+
+        return;
     }
 
     /*
@@ -87,6 +140,71 @@ public class Main {
      * Print commit stats
      */
     public static void stop() {
+        String homeDir = System.getProperty("user.home");
+        String pathToDayTime = homeDir + DAY_FILE_PATH;
+        String pathToMonthTime = homeDir + MONTH_FILE_PATH;
+        String pathToYesterdayTime = homeDir + YESTERDAY_FILE_PATH;
+
+        Timer sessionTimer = new Timer(homeDir + SESSION_FILE_PATH);
+        Timer dayTimer = new Timer(pathToDayTime);
+        Timer monthTimer = new Timer(pathToMonthTime);
+        Timer yesterdayTimer = new Timer(pathToYesterdayTime);
+
+        long duration = sessionTimer.getSessionTime(); 
+
+        if(dayTimer.fileIsNotExist()) {
+            dayTimer.writeToFile(0L);
+        }
+
+        long dayTime = dayTimer.readFromFile() + duration;
+
+        dayTimer.writeToFile(dayTime);
+
+        if(monthTimer.fileIsNotExist()) {
+            monthTimer.writeToFile(0L);
+        }
+        long monthTime = monthTimer.readFromFile();
+
+        long yesterdayTime = yesterdayTimer.readFromFile();
+
+        long sessionHours = duration / 3600;
+        long sessionMinutes = (duration % 3600) / 60;
+        long sessionSeconds = duration % 60;
+
+        long dayHours = dayTime / 3600;
+        long dayMinutes = (dayTime % 3600) / 60;
+        long daySeconds = dayTime % 60;
+
+        long monthHours = monthTime / 3600;
+        long monthMinutes = (monthTime % 3600) / 60;
+        long monthSeconds = monthTime % 60;
+
+        System.out.println("\n");
+        System.out.print("""
+                =========================================
+                Vim uptime:                
+                -----------------------------------------
+                """);
+        System.out.printf( "  - per session:        %2d h %2d min %2d sec\n",
+                sessionHours, sessionMinutes, sessionSeconds);
+        System.out.printf( "  - per day:            %2d h %2d min %2d sec\n",
+                dayHours, dayMinutes, daySeconds);
+        if(monthTime > yesterdayTime || dayTime > yesterdayTime) {
+            System.out.printf(Colors.GREEN.apply(
+                        "  - average per month:  %2d h %2d min %2d sec\n"),
+                    monthHours, monthMinutes, monthSeconds);
+        }
+        else {
+            System.out.printf(Colors.RED.apply(
+                        "  - average per month:  %2d h %2d min %2d sec\n"),
+                    monthHours, monthMinutes, monthSeconds);
+        }
+
+        System.out.println("  =========================================");
+
+        sessionTimer.deleteFile();
+
+
         CommitStats commitStats = createCommitStats(); 
         long savedDailyCommits = commitStats.readDailyCommitsFromFile();
 
@@ -94,9 +212,9 @@ public class Main {
             try {
                 Thread.sleep(200);
                 System.out.print("""
-                          =========================================
-                                        Commit stats:                
-                          -----------------------------------------
+                        =========================================
+                        Commit stats:                
+                        -----------------------------------------
                         """);
                 System.out.println("  - Commits per day: " + savedDailyCommits);
                 System.out.println("  =========================================");
@@ -106,3 +224,4 @@ public class Main {
         }).start();
     }
 }
+
